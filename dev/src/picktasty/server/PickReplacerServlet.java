f9131352c121fdf8c39eb695519e0c0fa2173436
Mario de Sa Vera <desavera@gmail.com>
2015-09-12 12:23:19 -0300

    Hello World !

--- dev/src/picktasty/server/PickReplacerServlet.java
+++ dev/src/picktasty/server/PickReplacerServlet.java
@@ -1,24 +1,20 @@
-package pictasty.server;
+package picktasty.server;
 
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-// Extend HttpServlet class
-public class PicReplacerServlet extends HttpServlet {
+public class PickReplacerServlet extends HttpServlet {
 
     private String logoName;
 
     public void init() throws ServletException {
-        // Do required initialization
         logoName = "Hello World";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Set response content type
         response.setContentType("text/html");
-        // Actual logic goes here.
         PrintWriter out = response.getWriter();
         out.println("<h1>" + logoName + "</h1>");
     }
